bebd208bd1e701122406412eaa1a92d088c78427
abraham sanchez fonseca <abraham_gto@hotmail.com>
2020-05-14 13:36:38 -0500

    Basic routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/pages/main/main.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { NavControlComponent } from './components/nav-control/nav-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    MainComponent,
+    NavControlComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
